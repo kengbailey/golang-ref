dc6d986c21dd0bc340ba3142f5e84634019f86cd
Ken Bailey <kgbailey92@gmail.com>
2018-12-16 17:47:47 -0500

    added sample of json unmarshall to golang struct

--- struct.go
+++ struct.go
@@ -1,25 +1,34 @@
 /*
-    Ken Bailey
-    3/12/18
+   Ken Bailey
+   3/12/18
 
-    All about structs in Go
+   All about structs in Go
 
 */
 
 package main
 
 import (
-    "fmt"
-    "reflect"
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
 )
 
 // Go has several built in types that most people are familiar with.
 // Go also allows you to create more complex types based on these foundational types
 type Person struct {
-    age         int
-    name        string
-    weight      float32
-    isJamaican  bool
+	age        int
+	name       string
+	weight     float32
+	isJamaican bool
+}
+
+type profile struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	DOB  string `json"dob"`
 }
 
 // Here we create a method for our Person struct.
@@ -28,7 +37,7 @@ type Person struct {
 // Methods with the same name can be defined on different types, while
 //  functions with the same name are not allowed
 func (p Person) Speak() {
-    fmt.Printf("My name is %s. I am %d years old and weigh %v pounds. Am I Jamaican? %t\n", p.name, p.age, p.weight, p.isJamaican) 
+	fmt.Printf("My name is %s. I am %d years old and weigh %v pounds. Am I Jamaican? %t\n", p.name, p.age, p.weight, p.isJamaican)
 }
 
 // Go allows us to create a type alias like this.
@@ -38,39 +47,51 @@ type OtherPerson Person
 
 // We can create another method of the same name, but with a different receiver type
 func (o OtherPerson) Speak() {
-    fmt.Printf("My name %s!\n", o.name)
+	fmt.Printf("My name %s!\n", o.name)
 }
 
 func main() {
-    // We can instantiate a Person object
-    //  and initialize its properties on the same line in the
-    //  order they are declared in the struct.
-    ken := Person{25, "ken", 175.5, true}
-    ken.Speak()
 
-    // We can also Instantiate a Person object
-    //  and initialize its properties one by one. Upon instantiation 
-    //  of an object without assigning its properties values, each 
-    //  property is initialized with a default value
-    karla := Person{}
-    karla.name = "karla"
-    fmt.Printf("%s: %d\n", karla.name, karla.age)
-    karla.age = 22
-    fmt.Printf("%s: %d\n", karla.name, karla.age)
+	// We can instantiate a Person object
+	//  and initialize its properties on the same line in the
+	//  order they are declared in the struct.
+	ken := Person{25, "ken", 175.5, true}
+	ken.Speak()
 
-    //  If we wanted to cast a one type into another we use "TYPE()".
-    //   We use reflection to get the the variable's type.
-    notKen := OtherPerson{90, "notken", 300.3, false}
-    notKen.Speak()
-    otherKen := Person(notKen)
-    fmt.Println("notKen type: ", reflect.TypeOf(notKen))
-    fmt.Println("otherKen type: ", reflect.TypeOf(otherKen))
+	// We can also Instantiate a Person object
+	//  and initialize its properties one by one. Upon instantiation
+	//  of an object without assigning its properties values, each
+	//  property is initialized with a default value
+	karla := Person{}
+	karla.name = "karla"
+	fmt.Printf("%s: %d\n", karla.name, karla.age)
+	karla.age = 22
+	fmt.Printf("%s: %d\n", karla.name, karla.age)
 
-    // Even though types Person and OtherPerson are stucturally similar,
-    //  we cant directly assign them to each other. Statements below are
-    //  illegal.
-    //
-    //  var newKen Person
-    //  newKen = notKen
+	//  If we wanted to cast a one type into another we use "TYPE()".
+	//   We use reflection to get the the variable's type.
+	notKen := OtherPerson{90, "notken", 300.3, false}
+	notKen.Speak()
+	otherKen := Person(notKen)
+	fmt.Println("notKen type: ", reflect.TypeOf(notKen))
+	fmt.Println("otherKen type: ", reflect.TypeOf(otherKen))
 
+	// Even though types Person and OtherPerson are stucturally similar,
+	//  we cant directly assign them to each other. Statements below are
+	//  illegal.
+	//
+	//  var newKen Person
+	//  newKen = notKen
+
+	// JSON to Golang struct
+	// The profile struct above has some extra information attached to each field.
+	// This helps translate json to golang struct
+
+	jsonText := []byte(`{"id":23, "name": "kenneth", "age": 26, "dob":"aug 9 1992"}`)
+	var prof profile
+	err := json.Unmarshal(jsonText, &prof)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(prof.Name)
 }
