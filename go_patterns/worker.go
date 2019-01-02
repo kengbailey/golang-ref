588d765fac10d9c70785db0bdea4c5c4914e7533
Ken Bailey <kgbailey92@gmail.com>
2019-01-02 08:08:34 -0600

    Added worker goroutine concurrency pattern

---
+++ go_patterns/worker.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("Worker", id, "started job", j)
+		time.Sleep(time.Second)
+		fmt.Println("Worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+
+func main() {
+	jobs := make(chan int, 100)    // channel to send work
+	results := make(chan int, 100) // channel to recieve results
+
+	// kick off workers
+	// worker will block until channel populates
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// send 5 jobs to the channel
+	// close channel to signal goroutine stop
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// read result from results channel
+	for a := 1; a <= 5; a++ {
+		fmt.Println(<-results)
+	}
+}
