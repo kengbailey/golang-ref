ffcb8f242c0fc0b2603497a8c63c947890325e96
Ken Bailey <kgbailey92@gmail.com>
2019-01-29 08:39:59 -0600

    added sync.WaitGroup pattern

--- concurrency_patterns/worker_pool.go
+++ concurrency_patterns/worker_pool.go
@@ -10,16 +10,18 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
 	for j := range jobs {
 		fmt.Println("Worker", id, "started job", j)
 		time.Sleep(time.Second)
 		fmt.Println("Worker", id, "finished job", j)
 		results <- j * 2
 	}
+	wg.Done()
 }
 
 func main() {
@@ -28,8 +30,11 @@ func main() {
 
 	// kick off workers
 	// worker will block until channel populates
+	// waitgroup signals that worker is done
+	var wg sync.WaitGroup
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		wg.Add(1)
+		go worker(w, jobs, results, &wg)
 	}
 
 	// send 5 jobs to the channel
@@ -38,6 +43,9 @@ func main() {
 		jobs <- j
 	}
 	close(jobs)
+
+	// wait for jobs to finish
+	wg.Wait()
 
 	// read result from results channel
 	for a := 1; a <= 5; a++ {
