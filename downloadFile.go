05dcf5225b46e84ecbd3af5bd59cff703d60fd47
Ken Bailey <kgbailey92@gmail.com>
2019-11-30 13:44:01 -0600

    added file downloader; using io.Copy()

---
+++ downloadFile.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	url := "https://images-na.ssl-images-amazon.com/images/I/8166xCVDGnL._SL1500_.jpg"
+	err := downloader("rubber_duck.jpg", url)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func downloader(destination, url string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	out, err := os.Create(destination)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// We use iocopy so the entire file is not loaded into memory
+	// "x" contains the number of bytes written to file
+	x, err := io.Copy(out, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%x kb file downloaded to : %s \n", x/1000, destination)
+
+	return err
+}
